b2cf2c89052b781f5aa9556c20f8c6deeeb86317
BCsabaEngine <csaba@balazs-magyar.hu>
2025-08-06 00:53:21 +0200

    chore: div smell

--- src/components/docs/DocsPageTitle.svelte
+++ src/components/docs/DocsPageTitle.svelte
@@ -11,14 +11,12 @@
 	const { title, subTitle, children }: Properties = $props();
 </script>
 
-<div class="text-center">
-	<Heading class="text-2xl font-bold" tag="h1">
-		{title}
-		{#if subTitle}
-			<Secondary class="ms-2 text-lg leading-loose font-extralight">{subTitle}</Secondary>
-		{/if}
-	</Heading>
-	{#if children}
-		<P class="text-lg dark:text-gray-400" space="wide">{@render children()}</P>
+<Heading class="text-2xl font-bold" tag="h1">
+	{title}
+	{#if subTitle}
+		<Secondary class="ms-2 text-lg leading-loose font-extralight">{subTitle}</Secondary>
 	{/if}
-</div>
+</Heading>
+{#if children}
+	<P class="text-lg dark:text-gray-400" space="wide">{@render children()}</P>
+{/if}
