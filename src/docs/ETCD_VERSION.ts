02b4bd9346abc6950d46e772c28c4a13fb18b804
BCsabaEngine <csaba@balazs-magyar.hu>
2025-08-12 12:43:19 +0200

    fix: update etcd version

--- src/docs/ETCD_VERSION.ts
+++ src/docs/ETCD_VERSION.ts
@@ -1,4 +1,2 @@
-// https://quay.io/repository/coreos/etcd?tab=tags
-
-export const ETCD_VERSION = '3.6.1-debian-12-r2';
+export const ETCD_VERSION = '3.6.4-debian-12-r2';
 export const ETCD_IMAGE = `bitnami/etcd:${ETCD_VERSION}`;
